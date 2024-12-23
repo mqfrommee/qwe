386f11917e259287e120d362c52015b4cd9bd0ec
mqfrommee <mqfromme@mail.ru>
2024-12-23 22:24:07 +0300

    окно

--- static/js/modal_home.js
+++ static/js/modal_home.js
@@ -5,21 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Открытие модального окна
     menuButton.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    document.body.classList.add('modal-open');
+        modal.classList.add('open');
+        document.body.classList.add('modal-open');
     });
 
     // Закрытие модального окна
     closeButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
-    document.body.classList.remove('modal-open');
+        modal.classList.remove('open');
+        document.body.classList.remove('modal-open');
     });
 
     // Закрытие модального окна при клике вне его
- window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        modal.classList.add('hidden');
-        document.body.classList.remove('modal-open');
-    }
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.classList.remove('open');
+            document.body.classList.remove('modal-open');
+        }
     });
 });
